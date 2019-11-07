acf51780f23cccdf54beced16486932436de140a
Utku Sarioglu <utkusarioglu@hotmail.com>
2019-11-07 22:49:42 +0300

    Switched to es6

--- lib-esm/c_namespace.js
+++ lib-esm/c_namespace.js
@@ -1,4 +1,4 @@
-export var C_Namespace = {
+export const C_Namespace = {
     E_AlreadyDefined: "%1 Namespace already defined",
     E_CalledBeforeDeclaration: "%1 Namespace called before declaration",
     E_NoEmpty: "%1 Namespace declaration cannot be empty",
